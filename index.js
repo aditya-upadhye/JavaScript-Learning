ae026994033f9710d99e9246c96ebc3557820aac
aditya-upadhye <adityaupadhye47@gmail.om>
2022-04-17 12:18:52 +0530

    Variables & Constants

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-// This is my first JavaScript code!
-console.log("Hello World");
+// VARIABLES:
+
+let name = "Aditya";
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen(-)
+// Are case-sensitive
+
+let firstName = "Aditya";
+let lastName = "Upadhye";
+
+// CONTANTS
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+// In this case, 1 will be printed.
+// Using `let`, the variable value can be changed later by decaring it again.
+// If you want to make the variable that should not change later, use `const`.
