ee0cfbcda2fe1d148a7fba80d4187adee31a831b
aditya-upadhye <adityaupadhye47@gmail.com>
2022-04-23 22:16:50 +0530

    Data Types and 'let', 'const' and 'var' keywords

---
+++ 02 - JavaScript Fundamentals - Part 1/script.js
@@ -0,0 +1,43 @@
+/* 
+
+let js = "amazing";
+console.log(48 + 8 + 23 - 10);
+
+console.log("Aditya");
+console.log(23);
+
+let firstName = "Aditya";
+console.log(firstName);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascript);
+console.log(typeof 23);
+console.log(typeof "Aditya");
+
+javascriptIsFun = "Yes!!!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2000;
+console.log(typeof year);
+
+console.log(typeof null);
+
+*/
+let age = 30;
+age = 31;
+
+const birthYear = 2003;
+// birthYear = 2002;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
