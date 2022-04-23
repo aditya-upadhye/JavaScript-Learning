8ba4eac7d843a1f1ab0901d09d0fcf53f5950767
aditya-upadhye <adityaupadhye47@gmail.com>
2022-04-23 23:58:05 +0530

    Basic Operators

--- 02 - JavaScript Fundamentals - Part 1/script.js
+++ 02 - JavaScript Fundamentals - Part 1/script.js
@@ -30,7 +30,6 @@ console.log(typeof year);
 
 console.log(typeof null);
 
-*/
 let age = 30;
 age = 31;
 
@@ -41,3 +40,25 @@ const birthYear = 2003;
 
 var job = "programmer";
 job = "teacher";
+
+*/
+
+const now = 2035;
+
+const ageAditya = now - 2003;
+const ageJohn = now - 2018;
+console.log(ageAditya, ageJohn);
+
+console.log(ageAditya * 2, ageAditya / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Aditya";
+const lastName = "Upadhye";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
