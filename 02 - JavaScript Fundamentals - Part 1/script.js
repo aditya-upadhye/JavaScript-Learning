8d5a80e73ad514f05af4d848c448b3607b493fc9
aditya-upadhye <adityaupadhye47@gmail.com>
2022-04-24 22:56:47 +0530

    Operator Precedence

--- 02 - JavaScript Fundamentals - Part 1/script.js
+++ 02 - JavaScript Fundamentals - Part 1/script.js
@@ -41,7 +41,6 @@ const birthYear = 2003;
 var job = "programmer";
 job = "teacher";
 
-*/
 
 const now = 2035;
 
@@ -62,3 +61,27 @@ x *= 4; // x = x * 4 = 100
 x++; // x = x + 1
 x--; // x = x - 1
 console.log(x);
+
+// Comparison operators
+// >, <, >=, <=
+console.log(ageAditya > ageJohn);
+console.log(ageAditya >= 18);
+
+const isFullAge = ageJohn >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+*/
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+const now = 2035;
+
+const ageAditya = now - 2003;
+const ageJohn = now - 2018;
+console.log(ageAditya, ageJohn);
+
+const averageAge = (ageAditya + ageJohn) / 2;
+console.log(ageAditya, ageJohn, averageAge);
